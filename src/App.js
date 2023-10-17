0ee787f265e745779c40ee23e92b942e0ad96ede
nikoroytio <roytioniko@gmail.com>
2023-10-17 14:47:20 +0300

    added SplashScreen to display before Hero section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
+import React, { useState } from 'react';
 import './App.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import SplashScreen from "./components/SplashScreen"
+import Hero from "./components/Hero";
+
 
 function App() {
+
+  const [showSplash, setShowSplash] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showSplash ? (
+        <SplashScreen onFinished={() => setShowSplash(false)} />
+      ) : (
+        <Hero />
+      )}
     </div>
   );
 }
 
 export default App;
+
