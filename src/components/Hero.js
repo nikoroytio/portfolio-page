a2421700d4309e46f47f83c019c066717a89e800
nikoroytio <roytioniko@gmail.com>
2023-10-17 14:08:47 +0300

    Added scroll down-button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import BackgroundVideo from './backgroundVideo';
-import './Hero.css';  
+import { Container, Row, Col } from 'react-bootstrap';
+import BackgroundVideo from "./BackgroundVideo"
+import scrollDownPng from '../assets/image/scrollDown.png';
+import './Hero.css';
 
 function Hero() {
   return (
-    <Container fluid className="p-0 hero-container">
+    <Container className="p-0 hero-container">
       <BackgroundVideo />
       <div className="overlay-content">
-        <h1>Your Name</h1>
-        <p>Your Profession</p>
-      </div>
+        <Row>
+        <Col md={6}>
+            <h1 className='typewriter-h1'>HEI, OLEN NIKO RÖYTIÖ</h1>
+        </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+        <h2 className='typewriter-h2'>TULEVA KOODARINNE</h2>
+        <button type='button' className='scrollDown'>
+        <img src={scrollDownPng} alt="Scroll Down" />
+        </button>
+          </Col>
+        </Row>
+        
+          
+    </div>  
     </Container>
   );
 }
