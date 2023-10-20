d0d3547c25e9e7b21004c9d297f5b46c03e3e6a7
nikoroytio <80318478+nikoroytio@users.noreply.github.com>
2023-10-20 11:28:20 +0300

    Add files via upload

---
+++ src/components/BackgroundVideo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./BackgroundVideo.css";
+import videoSrc from '../assets/videos/oma-kuva.mp4';
+
+// This functional component renders a video that plays automatically in the background of hero section
+function BackgroundVideo() {
+  return (
+    <div className="video-container">
+      <video autoPlay="autoplay" muted className="video">
+        <source src={videoSrc} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+}
+
+export default BackgroundVideo;
+
