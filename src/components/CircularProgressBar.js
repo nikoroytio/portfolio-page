fee4bd83fdaa6c9094cdcd5ef168204043195575
nikoroytio <roytioniko@gmail.com>
2023-10-20 11:36:03 +0300

    added comments

--- src/components/CircularProgressBar.js
+++ src/components/CircularProgressBar.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+// This component displays a circular progress bar with a given value
 const CircularProgressBar = ({ value }) => {
   const radius = 40;
   const circumference = 2 * Math.PI * radius;
+  // Calculate offset based on value to create progress effect
   const offset = circumference - (value / 100) * circumference;
 
   return (
@@ -11,7 +13,7 @@ const CircularProgressBar = ({ value }) => {
         cx="50"
         cy="50"
         r={radius}
-        stroke="grey"
+        stroke="rgb(20, 20, 20)"
         strokeWidth="4"
         fill="transparent"
       />
@@ -19,12 +21,15 @@ const CircularProgressBar = ({ value }) => {
         cx="50"
         cy="50"
         r={radius}
-        stroke="blue"
+        stroke="#f9004d"
         strokeWidth="4"
         fill="transparent"
         strokeDasharray={circumference}
-        strokeDashoffset={offset}
+        strokeDashoffset={offset}s
       />
+       <text x="50%" y="50%" dy=".3em" textAnchor="middle" fontSize="20px" fill="white">
+        {`${value}%`}
+      </text>
     </svg>
   );
 };
