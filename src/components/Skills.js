d2035e44f0980c02abe04241dd56e966c2f388ff
nikoroytio <roytioniko@gmail.com>
2023-10-23 10:08:01 +0300

    cahnged skill section to be it's own component

---
+++ src/components/Skills.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import CircularProgressBar from './CircularProgressBar';
+
+function Skills() {
+  return (
+    <Container >
+              <Row>
+                <Col>
+                  <CircularProgressBar value={42}/>
+                  <p>JAVASCRIPT</p>
+                </Col>
+                <Col>
+                  <CircularProgressBar value={64}/>
+                  <p>HTML</p>
+                </Col>
+                <Col>
+                  <CircularProgressBar value={63}/>
+                  <p>CSS</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <CircularProgressBar value={30}/>
+                  <p>PYTHON</p>
+                </Col>
+                <Col>
+                  <CircularProgressBar value={100}/>
+                  <p>VIDEO EDITING</p>
+                </Col>
+                <Col>
+                  <CircularProgressBar value={83}/>
+                  <p>GRAPHIC DESIGN</p>
+                </Col>
+              </Row>
+          </Container>
+  )
+}
+
+export default Skills
