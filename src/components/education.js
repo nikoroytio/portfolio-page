f30acc83ff9f2c12ebe1248a679b1d88b4ea517f
nikoroytio <roytioniko@gmail.com>
2023-10-23 09:54:47 +0300

    changed education part to beactual component

---
+++ src/components/education.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+
+function Education() {
+  return (
+    <Container>
+        <Row> 
+            <h4>2014-2017</h4>
+            <h3>Bachelor of Sport Studies -Haaga-Helia - university of applied sciences</h3>
+            <h4>2020 - present</h4>
+            <h3>Bachelor of Engineering, Information Technology - LAB - university of applied sciences</h3>
+        </Row>
+    </Container>
+  )
+}
+
+export default Education
