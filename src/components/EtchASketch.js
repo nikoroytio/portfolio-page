8c50b106b161b787efc49f1c9cc03e3c84f40caf
nikoroytio <roytioniko@gmail.com>
2023-10-25 14:18:11 +0300

    added functionality to drawn when click is pressed

--- src/components/EtchASketch.js
+++ src/components/EtchASketch.js
@@ -4,17 +4,19 @@ import './EtchASketch.css';
 function EtchASketch() {
     const [squares, setSquares] = useState(Array(16 * 16).fill(false));
     const [gridSize, setGridSize] = useState(16);
-
+    const [isDrawing, setIsDrawing] = useState(false); // To track if mouse button is pressed
     const [isErasing, setIsErasing] = useState(false);
 
     const handleMouseOver = (index) => {
-        const updatedSquares = [...squares];
-        if (isErasing) {
-            updatedSquares[index] = false; // Erase
-        } else {
-            updatedSquares[index] = true;  // Draw
+        if (isDrawing) {
+            const updatedSquares = [...squares];
+            if (isErasing) {
+                updatedSquares[index] = false; // Erase
+            } else {
+                updatedSquares[index] = true;  // Draw
+            }
+            setSquares(updatedSquares);
         }
-        setSquares(updatedSquares);
     };
 
     const resetSketchboard = () => {
@@ -39,7 +41,12 @@ function EtchASketch() {
             <button id="reset" onClick={resetSketchboard}>Make new sketchboard</button>
             <button onClick={() => setIsErasing(!isErasing)}> {isErasing ? "Switch to Draw Mode" : "Switch to Erase Mode"}</button>
             </div>
-            <div id="containerSketch" style={{ gridTemplateColumns: `repeat(${gridSize}, 1fr)` }}>
+            <div id="containerSketch" 
+                style={{ gridTemplateColumns: `repeat(${gridSize}, 1fr)` }}
+                onMouseDown={() => setIsDrawing(true)}  // Start drawing when mouse button is pressed
+                onMouseUp={() => setIsDrawing(false)}  // Stop drawing when mouse button is released
+                onMouseLeave={() => setIsDrawing(false)}  // Stop drawing if mouse leaves the container
+                >
                 {squares.map((isDrawn, index) => (
                     <div
                         key={index}
