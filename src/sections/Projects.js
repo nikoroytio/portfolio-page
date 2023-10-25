c82ccced03a52edb63db81b7a3f3096ecafd50d8
nikoroytio <roytioniko@gmail.com>
2023-10-25 14:02:00 +0300

    Added fadein animation to projects

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
+import { useInView } from 'react-intersection-observer';
 import Calculator from "../components/Calculator.js"
 import EtchASketch from "../components/EtchASketch.js"
 import RockPaperScissors from "../components/RockPaperScissors.js"
 import "./Projects.css"
 
+
+
 function Projects() {
+
+  const [ref, inView] = useInView({
+    threshold: 0.3,  // The percentage of the target element which is visible to trigger 'inView'
+    triggerOnce: true  // Ensures the animation only occurs once
+  });
+
   return (
-    <Container fluid className='projectContainer'>
+    <Container fluid className="projectContainer">
       <Row>
         <Col>
         <h2>My projects</h2>
         </Col>
       </Row>
-        <Row className='projectRow'>
+        <Row
+        className={`projectRow ${inView ? 'slideFadeInEffect' : ''}`}
+        ref={ref}
+        >
             <Col>
                 <h4>Calculator</h4>
                 <Calculator />
