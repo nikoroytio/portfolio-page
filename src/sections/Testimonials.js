e13a0f37def2b723b3c05b7c388a6d5b392176ee
nikoroytio <roytioniko@gmail.com>
2023-10-27 11:57:16 +0300

    changed classnames

--- src/sections/Testimonials.js
+++ src/sections/Testimonials.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
+import Testimonialsslider from "../components/Testimonialsslider"
 import "./Testimonials.css"
 
 function Testimonials() {
   return (
     <Container fluid>
-        <Row className='testimonialsContent'>
+        <Row className='testimonialsTitle'>
             <Col className='testimonialsItems'>
                 <h2>Working with me</h2>
             </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Testimonialsslider />
+          </Col>
         </Row>
     </Container>
   )
