1c8b60a6d0ad3bd99c892d66ca429c2f685f1c44
nikoroytio <roytioniko@gmail.com>
2023-10-29 07:47:57 +0200

    added placeholders for more testimonials

--- src/components/Testimonialsslider.js
+++ src/components/Testimonialsslider.js
@@ -19,6 +19,58 @@ function Testimonialsslider() {
               
             </Carousel.Caption>
           </Carousel.Item>
+          <Carousel.Item>
+            <img src={TestimonialsImage} alt="First slide" />
+            <Carousel.Caption>
+              <img src={QuotationMarks} />
+                <p>Cooperation with Niko has been rewarding. The contents of the trainings have been exactly 
+                  what was agreed and the micro-enterprises that were the target group have received tools for 
+                  developing their business from the trainings. Trainer Niko is approachable and you dare to ask 
+                  him any questions that come to mind. The atmosphere of the trainings has 
+                  been relaxed and immediate.</p>
+                  <i>Mia Koivujoki, R&D expert, Savonia University of Applied Sciences</i>
+              
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={TestimonialsImage} alt="First slide" />
+            <Carousel.Caption>
+              <img src={QuotationMarks} />
+                <p>Cooperation with Niko has been rewarding. The contents of the trainings have been exactly 
+                  what was agreed and the micro-enterprises that were the target group have received tools for 
+                  developing their business from the trainings. Trainer Niko is approachable and you dare to ask 
+                  him any questions that come to mind. The atmosphere of the trainings has 
+                  been relaxed and immediate.</p>
+                  <i>Sanna Hujanen, R&D expert, Savonia University of Applied Sciences</i>
+              
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={TestimonialsImage} alt="First slide" />
+            <Carousel.Caption>
+              <img src={QuotationMarks} />
+                <p>Cooperation with Niko has been rewarding. The contents of the trainings have been exactly 
+                  what was agreed and the micro-enterprises that were the target group have received tools for 
+                  developing their business from the trainings. Trainer Niko is approachable and you dare to ask 
+                  him any questions that come to mind. The atmosphere of the trainings has 
+                  been relaxed and immediate.</p>
+                  <i>Reetta Simppanen, R&D expert, Savonia University of Applied Sciences</i>
+              
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src={TestimonialsImage} alt="First slide" />
+            <Carousel.Caption>
+              <img src={QuotationMarks} />
+                <p>Cooperation with Niko has been rewarding. The contents of the trainings have been exactly 
+                  what was agreed and the micro-enterprises that were the target group have received tools for 
+                  developing their business from the trainings. Trainer Niko is approachable and you dare to ask 
+                  him any questions that come to mind. The atmosphere of the trainings has 
+                  been relaxed and immediate.</p>
+                  <i>Anna-Reetta Väänänen, R&D expert, Savonia University of Applied Sciences</i>
+              
+            </Carousel.Caption>
+          </Carousel.Item>
         </Carousel>
       );
 }
