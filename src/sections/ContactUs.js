4fca1de194622a2f24d749a2484a88b299ff774f
nikoroytio <roytioniko@gmail.com>
2023-10-30 13:00:37 +0200

    justified content from classname

--- src/sections/ContactUs.js
+++ src/sections/ContactUs.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import ContactForm from '../components/ContactForm';
+import ContactImage from '../assets/image/niko-roytio-contact.webp'
 import "./ContactUs.css"
 
 function ContactUs() {
   return (
-    <Container fluid className='contactUs'>
-        <Row>
-            <Col>
-            
+    <Container fluid className='p-16 contactUs'>
+        <Row className='justify-content-center'>
+            <Col md={4}>
+             <img src={ContactImage} alt="picture of niko röytiö"></img>
             </Col>
-            <Col>
+            <Col md={4}>
                 <ContactForm/>
             </Col>
         </Row>
