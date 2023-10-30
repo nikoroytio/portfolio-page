a8b6ad1ad5f97cdf52e339c915d8967744e336e6
nikoroytio <roytioniko@gmail.com>
2023-10-30 13:28:44 +0200

    added fadeinview animation to section

--- src/sections/ContactUs.js
+++ src/sections/ContactUs.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
+import { useInView } from 'react-intersection-observer';
 import ContactForm from '../components/ContactForm';
 import ContactImage from '../assets/image/niko-roytio-contact.webp'
 import "./ContactUs.css"
 
 function ContactUs() {
+  const [ref, inView] = useInView({
+    threshold: 0.1,  // The percentage of the target element which is visible to trigger 'inView'
+    triggerOnce: true  // Ensures the animation only occurs once
+  });
+
   return (
-    <Container fluid className='p-16 contactUs'>
+    <Container fluid className={`p-16 contactUs ${inView ? 'slideFadeInEffect' : ''}`}
+    ref={ref}>
         <Row className='justify-content-center'>
             <Col md={4}>
              <img src={ContactImage} alt="picture of niko röytiö"></img>
