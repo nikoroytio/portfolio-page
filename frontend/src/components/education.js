6e011c942894d8ee964f5b8298a2c805ba90bb21
nikoroytio <roytioniko@gmail.com>
2023-10-30 14:00:59 +0200

    added column to structure

--- frontend/src/components/education.js
+++ frontend/src/components/education.js
@@ -5,10 +5,12 @@ function Education() {
   return (
     <Container>
         <Row> 
-            <h4>2014-2017</h4>
-            <h3>Bachelor of Sport Studies -Haaga-Helia - university of applied sciences</h3>
-            <h4>2020 - present</h4>
-            <h3>Bachelor of Engineering, Information Technology - LAB - university of applied sciences</h3>
+          <Col>
+              <h4>2014-2017</h4>
+              <h3>Bachelor of Sport Studies -Haaga-Helia - university of applied sciences</h3>
+              <h4>2020 - present</h4>
+              <h3>Bachelor of Engineering, Information Technology - LAB - university of applied sciences</h3>
+            </Col>
         </Row>
     </Container>
   )
