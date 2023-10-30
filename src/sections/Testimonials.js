4d85cb5eb701265de991db4a41db5cf09e9e2f18
nikoroytio <roytioniko@gmail.com>
2023-10-30 13:29:06 +0200

    added fadeinview animation to section

--- src/sections/Testimonials.js
+++ src/sections/Testimonials.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
+import { useInView } from 'react-intersection-observer';
 import Testimonialsslider from "../components/Testimonialsslider"
 import "./Testimonials.css"
 
 function Testimonials() {
+
+  const [ref, inView] = useInView({
+    threshold: 0.1,  // The percentage of the target element which is visible to trigger 'inView'
+    triggerOnce: true  // Ensures the animation only occurs once
+  });
+
   return (
-    <Container fluid>
+    <Container fluid className={`testimonialsContainer ${inView ? 'slideFadeInEffect' : ''}`}
+    ref={ref}
+    >
         <Row className='testimonialsTitle'>
             <Col className='testimonialsItems'>
                 <h2>Working with me</h2>
