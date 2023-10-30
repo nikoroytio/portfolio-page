a6e333e3fa10f4aaeb56ff9d6efe9dce011449b1
nikoroytio <roytioniko@gmail.com>
2023-10-30 13:30:07 +0200

    added paragraph text

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -19,39 +19,39 @@ function ContactForm() {
         <Row>
             <Col className='formArea'>
                 <h2>Hire me!</h2>
-                <p>I am available for internship or hire. Connect with me via phone: +358 40 546 8496 or email: roytioniko@gmail.com</p>
+                <p>I am available for internship or for hire. Connect with me via phone: +358 40 546 8496 or email: roytioniko@gmail.com</p>
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                            <FloatingLabel controlId="emailInput" className="mb-3" label="Email">
+                            <FloatingLabel controlId="emailInput" className="mb-3" label="Email*">
 
                                     <Form.Control
                                         required
                                         type="email"
-                                        placeholder="Email"
+                                        placeholder="Email*"
                                     />
                             </FloatingLabel>
 
-                        <FloatingLabel  controlId="nameInput" className="mb-3" label="Name">
+                        <FloatingLabel  controlId="nameInput" className="mb-3" label="Name*">
                                 <Form.Control
                                     required
                                     type="text"
-                                    placeholder="Name"
+                                    placeholder="Name*"
                                 />
                         </FloatingLabel>
-                        <FloatingLabel controlId="subjectInput" className="mb-3" label="Subject">
+                        <FloatingLabel controlId="subjectInput" className="mb-3" label="Subject*">
 
                                 <Form.Control
                                     required
                                     type="text"
-                                    placeholder="Subject"
+                                    placeholder="Subject*"
                                 />
                         </FloatingLabel>
-                        <FloatingLabel controlId="messageInput" className="mb-3" label="Message">
+                        <FloatingLabel controlId="messageInput" className="mb-3" label="Message*">
 
                                 <Form.Control
                                     required
                                     type="textarea"
                                     style={{ height: '100px' }}
-                                    placeholder="Message"
+                                    placeholder="Message*"
                                 />
                         </FloatingLabel>
 
