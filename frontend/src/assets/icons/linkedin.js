2db2aca88a3a7efc52db268bfbbf0a9d0e40c970
nikoroytio <roytioniko@gmail.com>
2023-10-31 14:44:19 +0200

    icons for social media banners

---
+++ frontend/src/assets/icons/linkedin.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function linkedin() {
+  return (
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="100%" viewBox="0 0 193 193" enable-background="new 0 0 193 193" xml:space="preserve">
+<path fill="#FFFFFF" opacity="1.000000" stroke="none" 
+	d="
+M193.469849,194.000000 
+	C128.959793,194.000000 64.979889,194.000000 1.015075,194.000000 
+	C1.015075,129.636490 1.015075,65.318245 1.015075,1.007517 
+	C65.353432,1.007517 129.676712,1.007517 194.000000,1.011275 
+	C194.000000,65.343353 194.000000,129.671677 193.469849,194.000000 
+z"/>
+</svg>
+  )
+}
+
+export default linkedin
