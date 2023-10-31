a50dcdb9d2e0fbbe9047cceb29f1f0483017c770
nikoroytio <roytioniko@gmail.com>
2023-10-31 10:31:39 +0200

    changed menu icon color

--- frontend/src/assets/icons/menu.js
+++ frontend/src/assets/icons/menu.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function menu() {
   return (
-    <svg viewBox="0 0 48 48" width="48" height="48">
+    <svg viewBox="0 0 48 48" width="48" height="48" >
       <g id="Hamburger_Menu">
-        <path d="M45.9,12h-44V8h44V12z M45.9,22h-44v4h44V22z M45.9,36h-44v4h44V36z"/>
+        <path fill="#f9004d" d="M45.9,12h-44V8h44V12z M45.9,22h-44v4h44V22z M45.9,36h-44v4h44V36z"/>
       </g>
     </svg>
   );
