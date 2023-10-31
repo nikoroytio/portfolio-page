8ee1a30b1b89a3b8ab3ffbe19d55b0474b313f23
nikoroytio <roytioniko@gmail.com>
2023-10-31 10:15:18 +0200

    initial commit

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
