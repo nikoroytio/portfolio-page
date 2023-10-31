b156f7e7bf5098e4509be9340fe1ce5f3aa95acd
nikoroytio <roytioniko@gmail.com>
2023-10-31 14:49:58 +0200

    added social media icons as imported svg

--- frontend/src/components/SocialMediaBanner.js
+++ frontend/src/components/SocialMediaBanner.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+import linkedin from '../assets/icons/linkedin';
+import github from '../assets/icons/github';
+import instagram from '../assets/icons/instagram';
 
 function SocialMediaBanner() {
   return (
-    <div>SocialMediaBanner</div>
+    <Container>
+        <Row>
+            <Col>
+            <a href="https://github.com/nikoroytio" target="_blank" rel="noopener noreferrer">
+              <img src={github} alt="Github - logo" />
+            </a>
+            <a href="https://www.instagram.com/rodelanka" target="_blank" rel="noopener noreferrer">
+              <img src={instagram} alt="Instagram - logo" />
+            </a>
+            <a href="https://www.linkedin.com/in/nikoroytio/" target="_blank" rel="noopener noreferrer">
+              <img src={linkedin} alt="LinkedIn - logo" />
+            </a>
+            </Col>
+        </Row>
+    </Container>
   )
 }
 
