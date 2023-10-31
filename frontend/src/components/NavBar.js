9e55de97dc0889a7a4f7047fb09d21f222e22ae8
nikoroytio <roytioniko@gmail.com>
2023-10-31 11:14:28 +0200

    added offcanvas container for menu items

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,17 +1,35 @@
-import React from 'react'
-import { Container,} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
-import Menu from '../assets/icons/menu'
-import './NavBar.css'
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Menu from '../assets/icons/menu';
+import './NavBar.css';
 
 function NavBar() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Navbar sticky="top">
-        <Container className='flex-row-reverse fade-in' >
-            <Menu/>
+    <div>
+      <Navbar sticky="top">
+        <Container className='flex-row-reverse fade-in'>
+          <Menu onClick={handleShow} />
         </Container>
-    </Navbar>
-  )
+      </Navbar>
+
+      <Offcanvas show={show} onHide={handleClose} placement="end">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
