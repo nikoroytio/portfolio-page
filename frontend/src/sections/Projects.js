1e8c7b6b92210e5745d1810ca6fd3318cda2b88b
nikoroytio <roytioniko@gmail.com>
2023-10-31 09:59:14 +0200

    added text

--- frontend/src/sections/Projects.js
+++ frontend/src/sections/Projects.js
@@ -44,7 +44,7 @@ function Projects() {
           <Col md={6}>
             <p>These are my example Javascript projects. I have originally made them with HTML, 
               CSS and Javascript and they can be found on my <a href="https://github.com/nikoroytio" target="_blank" rel="noreferrer">GitHub</a> . But for this page I converted 
-              them to react-components, so I could display them interactively. Apart from these projects 
+              them to react-components, so I could display them interactively. Also this page is an project which I made with React and Node.js. Apart from these projects 
               I have made number of Wordpress sites (like <a href="https://www.beautyparlour.fi/" target="_blank" rel="noreferrer">this </a> 
               for example), and have made code snippets for different projects, like for raspberry pi built weather station
               and backend function for wix website to use custom emails as validation for new registrars.</p>
