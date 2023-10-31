29c4b2cc4bc261b4b30319651aaa6193557a7bd1
nikoroytio <roytioniko@gmail.com>
2023-10-31 10:54:23 +0200

    added fade in animation to bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { Container,} from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 import Menu from '../assets/icons/menu'
+import './NavBar.css'
 
 function NavBar() {
   return (
     <Navbar sticky="top">
-        <Container className='flex-row-reverse' >
+        <Container className='flex-row-reverse fade-in' >
             <Menu/>
         </Container>
-    </Navbar >
+    </Navbar>
   )
 }
 
