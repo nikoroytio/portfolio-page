9b98ec28c2dbe48bc2a85251a67a623df22643a7
nikoroytio <roytioniko@gmail.com>
2023-10-31 11:07:24 +0200

    added button functionality to icon

--- frontend/src/assets/icons/menu.js
+++ frontend/src/assets/icons/menu.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
-function menu() {
+function Menu(props) {
   return (
-    <svg viewBox="0 0 48 48" width="48" height="48" >
-      <g id="Hamburger_Menu">
-        <path fill="#f9004d" d="M45.9,12h-44V8h44V12z M45.9,22h-44v4h44V22z M45.9,36h-44v4h44V36z"/>
-      </g>
-    </svg>
+    <button onClick={props.onClick} className="hamburger-button">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="48" height="48">
+        <g id="Hamburger_Menu">
+          <path fill="#f9004d" d="M45.9,12h-44V8h44V12z M45.9,22h-44v4h44V22z M45.9,36h-44v4h44V36z"/>
+        </g>
+      </svg>
+    </button>
   );
 }
 
-export default menu
+export default Menu;
