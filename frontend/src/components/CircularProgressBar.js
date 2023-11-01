4c1256e4d3d1f6f340a5e6bc5ffaf048bfd25d87
nikoroytio <roytioniko@gmail.com>
2023-11-01 19:48:03 +0200

    added viewBow attribute

--- frontend/src/components/CircularProgressBar.js
+++ frontend/src/components/CircularProgressBar.js
@@ -8,7 +8,7 @@ const CircularProgressBar = ({ value }) => {
   const offset = circumference - (value / 100) * circumference;
 
   return (
-    <svg width="100" height="100">
+    <svg width="100" height="100" viewBox="0 0 100 100">
       <circle
         cx="50"
         cy="50"
