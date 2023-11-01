3374eb9069f4c2484479bc0d4bdb201fcd0318b8
nikoroytio <roytioniko@gmail.com>
2023-11-01 14:47:39 +0200

    added copyright textto footer

--- frontend/src/sections/Footer.js
+++ frontend/src/sections/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import SocialMediaBanner from '../components/SocialMediaBanner';
+import './Footer.css'
 
 function Footer() {
   return (
     <Container>
-        <Row>
+        <Row className='footerContainer'>
             <Col>
+            <p>&copy; Niko Röytiö 2023</p>
             </Col>
             <Col>
               <SocialMediaBanner/>
