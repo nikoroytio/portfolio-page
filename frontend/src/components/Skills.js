7b0927c4ab44922cb3f536879d606ee5abb28541
nikoroytio <roytioniko@gmail.com>
2023-11-01 18:38:48 +0200

    changed column structure

--- frontend/src/components/Skills.js
+++ frontend/src/components/Skills.js
@@ -1,40 +1,31 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import CircularProgressBar from './CircularProgressBar';
-import './Skills.css'
 
 function Skills() {
   return (
     <Container className='skillBars'>
-              <Row>
-                <Col>
-                  <CircularProgressBar value={42}/>
-                  <p>JAVASCRIPT</p>
-                </Col>
-                <Col>
-                  <CircularProgressBar value={64}/>
-                  <p>HTML</p>
-                </Col>
-                <Col>
-                  <CircularProgressBar value={63}/>
-                  <p>CSS</p>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <CircularProgressBar value={30}/>
-                  <p>PYTHON</p>
-                </Col>
-                <Col>
-                  <CircularProgressBar value={100}/>
-                  <p>VIDEO EDITING</p>
-                </Col>
-                <Col>
-                  <CircularProgressBar value={83}/>
-                  <p>GRAPHIC DESIGN</p>
-                </Col>
-              </Row>
-          </Container>
+      <Row>
+        <Col>
+          <CircularProgressBar value={75} />
+          <p>CSS</p>
+          <CircularProgressBar value={84} />
+          <p>HTML</p>
+        </Col>
+        <Col>
+          <CircularProgressBar value={74} />
+          <p>JAVASCRIPT</p>
+          <CircularProgressBar value={52} />
+          <p>REACT</p>
+        </Col>
+        <Col>
+          <CircularProgressBar value={100} />
+          <p>VIDEO EDITING</p>
+          <CircularProgressBar value={83} />
+          <p>GRAPHIC DESIGN</p>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
