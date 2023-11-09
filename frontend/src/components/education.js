25a453ee1e46b07c276a8376a53ec9e4707dd4cf
nikoroytio <roytioniko@gmail.com>
2023-11-09 17:58:25 +0200

    changed order of education texts

--- frontend/src/components/education.js
+++ frontend/src/components/education.js
@@ -6,10 +6,10 @@ function Education() {
     <Container>
         <Row> 
           <Col>
-              <h4>2014-2017</h4>
-              <h3>Bachelor of Sport Studies -Haaga-Helia - university of applied sciences</h3>
               <h4>2020 - present</h4>
               <h3>Bachelor of Engineering, Information Technology - LAB - university of applied sciences</h3>
+              <h4>2014-2017</h4>
+              <h3>Bachelor of Sport Studies -Haaga-Helia - university of applied sciences</h3>
             </Col>
         </Row>
     </Container>
